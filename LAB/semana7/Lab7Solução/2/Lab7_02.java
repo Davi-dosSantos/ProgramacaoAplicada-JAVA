783ff57a63edcf4f66d5568d3f08ea48525164dd
Davi Costa <daviita1@hotmail.com>
2021-11-07 17:35:52 -0300

    lab done

--- LAB/semana7/Lab7Solução/2/Lab7_02.java
+++ LAB/semana7/Lab7Solução/2/Lab7_02.java
@@ -1,15 +1,13 @@
 
 import javax.swing.JFrame;
 
-public class Lab7_02
-{
+public class Lab7_02 {
    // execute application
-   public static void main(String[] args)
-   {
-      JFrame janela =  new JFrame ("teste slider");
-	  janela.add( new SliderPanel());
-	  janela.setSize(400,150);
-	  janela.setVisible(true);
+   public static void main(String[] args) {
+      JFrame janela = new JFrame("teste slider");
+      janela.add(new SliderPanel());
+      janela.setSize(400, 150);
+      janela.setVisible(true);
       janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-   } 
+   }
 } // end class Lab7_02
---
+++ LAB/semana7/Lab7Solução/3/MyColorChooser.java
@@ -0,0 +1,50 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class MyColorChooser extends JPanel {
+   private SliderPanel sliderR;
+   private SliderPanel sliderG;
+   private SliderPanel sliderB;
+
+   public MyColorChooser() {
+      setLayout(new GridLayout(3, 1));
+
+      sliderR = new SliderPanel();
+      sliderG = new SliderPanel();
+      sliderB = new SliderPanel();
+
+      add(sliderR);
+      add(sliderG);
+      add(sliderB);
+
+   }
+
+   // used by layout manager to determine preferred size
+   public Dimension getPreferredSize() {
+      return new Dimension(400, 150);
+   }
+
+   // used by layout manager to determine minimum size
+   public Dimension getMinimumSize() {
+      return getPreferredSize();
+   }
+
+   public Color getColor() {
+      Color cor = new Color(sliderR.getValue(), sliderG.getValue(), sliderB.getValue());
+      return cor;
+   }
+
+   // main de teste
+   // execute application
+   public static void main(String[] args) {
+      JFrame janela = new JFrame("teste MyColorChooser");
+      janela.add(new MyColorChooser());
+      janela.setSize(400, 450);
+      janela.setVisible(true);
+      janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   }
+
+} // end class MyColorChooser
